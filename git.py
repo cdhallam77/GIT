215182b04a06695f5d7ae83a9f5b14b83d948e98
nomascus <cdhallam77@gmail.com>
2017-11-28 14:39:47 +1100

    added some variables

---
+++ git.py
@@ -0,0 +1,25 @@
+# def addition(a, b):
+a=1
+b=2
+ 	summation = a + b
+
+ 	return summation
+
+
+def subtraction(a, b):
+
+# 	difference = a - b
+
+# 	return difference
+
+
+# def division(a, b):
+
+# 	quotient = a / b
+
+# 	return quotient
+
+
+# def multiplication(a, b):
+
+# 	product = a * b
