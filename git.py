bfe1234bd759fea2ec92100efcf7175ea5b63a74
nomascus <cdhallam77@gmail.com>
2017-11-28 14:51:37 +1100

    Just added text
    Text to indicate that I have written new stuff

--- git.py
+++ git.py
@@ -1,12 +1,13 @@
 # def addition(a, b):
-a=2
-b=4
+a=1
+b=2
  	summation = a + b
 
  	return summation
 
+Oh wow look at this new bit of text im writing...
 
-def subtraction(a, b):
+#def subtraction(a, b):
 
 # 	difference = a - b
 
